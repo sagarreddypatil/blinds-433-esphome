1f6c6d6becf807ca41988135d257fb72af0240e7
Sagar Patil <sagarreddypatil@gmail.com>
2026-01-04 15:22:22 -0500

    stuff

--- components/blinds_433/blinds_433.cpp
+++ components/blinds_433/blinds_433.cpp
@@ -11,6 +11,9 @@ static constexpr uint32_t HALF_BIT_US = 640;
 static constexpr uint32_t PREAMBLE_US = 6800;
 static constexpr uint32_t FRAME_GAP_US = 25000;
 
+// Minimum time between commands (ms) - accounts for full transmission + margin
+static constexpr uint32_t COMMAND_SPACING_MS = 500;
+
 static constexpr uint8_t CMD_STOP = 0x01;
 static constexpr uint8_t CMD_UP = 0x02;
 static constexpr uint8_t CMD_DOWN = 0x04;
@@ -21,44 +24,78 @@ void Blinds433Hub::setup() {
   ESP_LOGCONFIG(TAG, "Blinds 433MHz hub initialized");
 }
 
-void Blinds433Hub::send_command(uint32_t remote_id, uint8_t blind_id, uint8_t cmd, int repeats) {
+void Blinds433Hub::loop() {
+  if (queue_.empty()) {
+    return;
+  }
+
+  uint32_t now = millis();
+  if (now - last_send_time_ < COMMAND_SPACING_MS) {
+    return;
+  }
+
+  QueuedCommand cmd = queue_.front();
+  queue_.erase(queue_.begin());
+
+  ESP_LOGD(TAG, "Processing queued command (remaining: %d)", queue_.size());
+  send_command_(cmd);
+  last_send_time_ = millis();
+}
+
+void Blinds433Hub::queue_command(uint32_t remote_id, uint8_t blind_id,
+                                 uint8_t cmd, int repeats) {
+  QueuedCommand qc;
+  qc.remote_id = remote_id;
+  qc.blind_id = blind_id;
+  qc.cmd = cmd;
+  qc.repeats = repeats;
+
+  queue_.push_back(qc);
+  ESP_LOGD(TAG,
+           "Queued command 0x%02X for remote 0x%06X blind %d (queue size: %d)",
+           cmd, remote_id, blind_id, queue_.size());
+}
+
+void Blinds433Hub::send_command_(const QueuedCommand &cmd) {
   // Initialize counter for this remote if not exists
-  if (counters_.find(remote_id) == counters_.end()) {
+  if (counters_.find(cmd.remote_id) == counters_.end()) {
     for (int i = 0; i < 16; i++) {
-      counters_[remote_id][i] = 0x0410;
+      counters_[cmd.remote_id][i] = 0x0410;
     }
   }
 
   uint8_t data[8];
   data[0] = 0x81;
-  data[1] = cmd;
+  data[1] = cmd.cmd;
 
-  uint16_t counter = counters_[remote_id][blind_id];
+  uint16_t counter = counters_[cmd.remote_id][cmd.blind_id];
   data[2] = counter >> 8;
   data[3] = counter & 0xFF;
-  counters_[remote_id][blind_id]++;
+  counters_[cmd.remote_id][cmd.blind_id]++;
 
-  data[4] = ((remote_id >> 12) & 0xF0) | (blind_id & 0x0F);
-  data[5] = (remote_id >> 8) & 0xFF;
-  data[6] = remote_id & 0xFF;
+  data[4] = ((cmd.remote_id >> 12) & 0xF0) | (cmd.blind_id & 0x0F);
+  data[5] = (cmd.remote_id >> 8) & 0xFF;
+  data[6] = cmd.remote_id & 0xFF;
 
-  data[7] = (data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + 0x2B) & 0xFF;
+  data[7] =
+      (data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + 0x2B) & 0xFF;
 
-  ESP_LOGD(TAG, "Sending command 0x%02X to remote 0x%06X blind %d", cmd, remote_id, blind_id);
+  ESP_LOGD(TAG, "Sending command 0x%02X to remote 0x%06X blind %d", cmd.cmd,
+           cmd.remote_id, cmd.blind_id);
 
-  for (int i = 0; i < repeats; i++) {
+  for (int i = 0; i < cmd.repeats; i++) {
     send_frame_(data, i);
     delayMicroseconds(FRAME_GAP_US);
   }
 }
 
 void Blinds433Hub::send_bit_(uint8_t b) {
-  if (b) {  // 1 = LOW-HIGH
+  if (b) { // 1 = LOW-HIGH
     pin_->digital_write(false);
     delayMicroseconds(HALF_BIT_US);
     pin_->digital_write(true);
     delayMicroseconds(HALF_BIT_US);
-  } else {  // 0 = HIGH-LOW
+  } else { // 0 = HIGH-LOW
     pin_->digital_write(true);
     delayMicroseconds(HALF_BIT_US);
     pin_->digital_write(false);
@@ -93,7 +130,7 @@ void Blinds433Hub::send_frame_(uint8_t *data, uint32_t frame_num) {
 
 cover::CoverTraits Blinds433Cover::get_traits() {
   auto traits = cover::CoverTraits();
-  traits.set_is_assumed_state(true);
+  traits.set_is_assumed_state(false);
   traits.set_supports_position(false);
   traits.set_supports_tilt(false);
   traits.set_supports_stop(true);
@@ -102,21 +139,17 @@ cover::CoverTraits Blinds433Cover::get_traits() {
 
 void Blinds433Cover::control(const cover::CoverCall &call) {
   if (call.get_stop()) {
-    hub_->send_command(remote_id_, blind_id_, CMD_STOP);
-    this->publish_state();
+    hub_->queue_command(remote_id_, blind_id_, CMD_STOP);
   }
   if (call.get_position().has_value()) {
     float pos = *call.get_position();
     if (pos == cover::COVER_OPEN) {
-      hub_->send_command(remote_id_, blind_id_, CMD_UP);
-      this->position = cover::COVER_OPEN;
+      hub_->queue_command(remote_id_, blind_id_, CMD_UP);
     } else if (pos == cover::COVER_CLOSED) {
-      hub_->send_command(remote_id_, blind_id_, CMD_DOWN);
-      this->position = cover::COVER_CLOSED;
+      hub_->queue_command(remote_id_, blind_id_, CMD_DOWN);
     }
-    this->publish_state();
   }
 }
 
-}  // namespace blinds_433
-}  // namespace esphome
+} // namespace blinds_433
+} // namespace esphome
--- components/blinds_433/blinds_433.h
+++ components/blinds_433/blinds_433.h
@@ -1,30 +1,43 @@
 #pragma once
 
+#include "esphome/components/cover/cover.h"
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
-#include "esphome/components/cover/cover.h"
 #include <map>
+#include <vector>
 
 namespace esphome {
 namespace blinds_433 {
 
+struct QueuedCommand {
+  uint32_t remote_id;
+  uint8_t blind_id;
+  uint8_t cmd;
+  int repeats;
+};
+
 class Blinds433Hub : public Component {
- public:
+public:
   void set_pin(GPIOPin *pin) { pin_ = pin; }
   void setup() override;
+  void loop() override;
   float get_setup_priority() const override { return setup_priority::HARDWARE; }
-  void send_command(uint32_t remote_id, uint8_t blind_id, uint8_t cmd, int repeats = 6);
+  void queue_command(uint32_t remote_id, uint8_t blind_id, uint8_t cmd,
+                     int repeats = 6);
 
- protected:
+protected:
+  void send_command_(const QueuedCommand &cmd);
   void send_bit_(uint8_t b);
   void send_frame_(uint8_t *data, uint32_t frame_num);
 
   GPIOPin *pin_;
   std::map<uint32_t, uint16_t[16]> counters_;
+  std::vector<QueuedCommand> queue_;
+  uint32_t last_send_time_{0};
 };
 
 class Blinds433Cover : public cover::Cover, public Component {
- public:
+public:
   void set_hub(Blinds433Hub *hub) { hub_ = hub; }
   void set_remote_id(uint32_t remote_id) { remote_id_ = remote_id; }
   void set_blind_id(uint8_t blind_id) { blind_id_ = blind_id; }
@@ -33,11 +46,11 @@ class Blinds433Cover : public cover::Cover, public Component {
   cover::CoverTraits get_traits() override;
   void control(const cover::CoverCall &call) override;
 
- protected:
+protected:
   Blinds433Hub *hub_;
   uint32_t remote_id_;
   uint8_t blind_id_;
 };
 
-}  // namespace blinds_433
-}  // namespace esphome
+} // namespace blinds_433
+} // namespace esphome
